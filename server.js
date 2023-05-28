0dbb68e000cbc738e337e20d0363b1a491711c74
srirang <srirang@leena.ai>
2023-05-28 13:09:16 +0530

    type(fix): fixed cors issue on socket

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-
+var io = require('socket.io')(http, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });
+var cors = require('cors');
+app.use(cors());
+app.options('*', cors());
 app.get('/', (req, res) => res.send('hello!'));
 
 io.on('connection', (socket) => {  
     console.log('a user connected'); 
- });
+    socket.on('message', (msg) => {
+        console.log(msg);
+        socket.broadcast.emit('message-broadcast', msg);
+    });
+});
 
- 
+
+
 http.listen(3000, () => {
 
   console.log('listening on *:3000');
